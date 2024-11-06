04bdd85ee7fa4093a167da58539a44b5e66a9a89
alvl.ivanov <alvl.ivanov@samolet.ru>
2024-11-06 17:07:33 +0300

    added ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       useDetails: true,
       showError: true
     }],
-    ['json'],
+    ['json', { outputFile: 'results.json' }]
     ['html']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
